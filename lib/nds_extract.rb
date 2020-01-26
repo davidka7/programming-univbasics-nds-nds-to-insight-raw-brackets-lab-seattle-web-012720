1b35c112979b554459c06220d4929863ef6fc0ec
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-26 01:28:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+<<<<<<< HEAD
   pp nds
   i=0 
   result = {}
@@ -15,6 +16,15 @@ def directors_totals(nds)
   element+=1
 end
 i+=1
+=======
+  require= "pp"
+  row_index=0 
+  result = {}
+  while row_index<nds.length do
+  result += nds[row_index]
+row_index+=1
+
+>>>>>>> 045788aaa48f4baf71d3f5309e2db911d9ca3d26
 end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -31,5 +41,9 @@ end
   #
   #
   # Be sure to return the result at the end!
+<<<<<<< HEAD
   return result
+=======
+  result
+>>>>>>> 045788aaa48f4baf71d3f5309e2db911d9ca3d26
 end
